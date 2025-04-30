005c5aee1f78c99c909463e514c1b2696585e44b
r2d2c2 <r2d2b22@gmail.com>
2025-04-30 09:19:47 +0900

    초기 커밋.

---
+++ WindowsFormsApp_01_if/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_01_if
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            RandomNumber(false);
+        }
+        bool RandomNumber(bool isinput)
+        {
+            int isZeroOne;
+            //int형 난수 생성
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100);
+            if (randomNumber % 2 == 0)
+            {
+                isZeroOne = 0;
+                if (isinput == false)
+                {
+                    MessageBox.Show("승리.");
+                }
+                else
+                {
+                    MessageBox.Show("패배.");
+                }
+            }
+            else
+            {
+                isZeroOne = 1;
+                if (isinput == false)
+                {
+                    MessageBox.Show("패배.");
+                }
+                else
+                {
+                    MessageBox.Show("승리.");
+                }
+            }
+            return isinput;
+        }
+    }
+}
